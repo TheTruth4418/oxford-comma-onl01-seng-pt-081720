9991191f0a2e4356319594096f14536bb7d8f291
William West <wwest466@gmail.com>
2020-08-19 14:34:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
     return array.join
+  elsif array == 2
+    return array.join(" and ")
   else
-  last = ", and " << array.pop()
+    last = ", and " << array.pop()
 
-  return array.join(", ") << last
+    return array.join(", ") << last
 end
 end
